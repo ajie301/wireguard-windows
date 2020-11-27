b97b689027866daee0d9477f260c78b13156bccc
fengtuo58 <fengtuo58@qq.com>
2020-11-27 15:27:00 +0800

    SDP-17053 排除ip在线推送，推送完毕后，客户端路由表中所有排除ip被清空 【现象】排除ip更新不生效 【原因】刷新时排除ip被清掉了但是没有加入新的排除配置 【方案】添加刷新新的排除ip配置

--- tunnel/watcher.go
+++ tunnel/watcher.go
@@ -101,4 +101,59 @@ func (w *Watcher) FlushConfig(conf *conf.Config, tun *tun.NativeTun) {
 	for _, rd := range deduplicatedRoutes {
 		luid.AddRoute(rd.Destination, rd.NextHop, rd.Metric)
 	}
+	// 添加excludeIp
+	estimatedRouteCount = 0
+	for _, peer := range conf.Peers {
+		estimatedRouteCount += len(peer.ExcludedIPs)
+	}
+	routes = make([]winipcfg.RouteData, 0, estimatedRouteCount)
+	addresses := make([]net.IPNet, len(conf.Interface.Addresses))
+	//var haveV4Address, haveV6Address bool
+	//判断地址是v6还是v4
+	for i, addr := range conf.Interface.Addresses {
+		addresses[i] = addr.IPNet()
+		if addr.Bits() == 32 {
+			haveV4Address = true
+		} else if addr.Bits() == 128 {
+			haveV6Address = true
+		}
+	}
+	//取出排除ip项构造设置路由表的数据
+	for _, peer := range conf.Peers {
+		for _, excluedip := range peer.ExcludedIPs {
+			if (excluedip.Bits() == 32 && !haveV4Address) || (excluedip.Bits() == 128 && !haveV6Address) {
+				continue
+			}
+			route := winipcfg.RouteData{
+				Destination: excluedip.IPNet(),
+				Metric:      0,
+			}
+			if excluedip.Bits() == 32 {
+				route.NextHop = net.IPv4zero
+			} else if excluedip.Bits() == 128 {
+				route.NextHop = net.IPv6zero
+			}
+			routes = append(routes, route)
+		}
+	}
+	// 去除重复排除ip项
+	deduplicatedRoutes = make([]*winipcfg.RouteData, 0, len(routes))
+	sort.Slice(routes, func(i, j int) bool {
+		return routes[i].Metric < routes[j].Metric ||
+			bytes.Compare(routes[i].NextHop, routes[j].NextHop) == -1 ||
+			bytes.Compare(routes[i].Destination.IP, routes[j].Destination.IP) == -1 ||
+			bytes.Compare(routes[i].Destination.Mask, routes[j].Destination.Mask) == -1
+	})
+	for i := 0; i < len(routes); i++ {
+		if i > 0 && routes[i].Metric == routes[i-1].Metric &&
+			bytes.Equal(routes[i].NextHop, routes[i-1].NextHop) &&
+			bytes.Equal(routes[i].Destination.IP, routes[i-1].Destination.IP) &&
+			bytes.Equal(routes[i].Destination.Mask, routes[i-1].Destination.Mask) {
+			continue
+		}
+		deduplicatedRoutes = append(deduplicatedRoutes, &routes[i])
+	}
+	//设置排除ip项，调用之前写的排除设置接口
+	SetDefaultRoutesForFamily(windows.AF_INET, w.watcher.tun, deduplicatedRoutes)
+
 }
